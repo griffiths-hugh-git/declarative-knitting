3ec25637b407b3cf9ed0eaffc7b050b200161454
griffiths.hugh <griffiths.hugh.git@gmail.com>
2018-05-11 21:26:51 +0100

    Moves Latex template content into config #2

--- declarative-knitting-core/src/main/java/com/griffiths/hugh/declarative_knitting/core/rendering/LatexRenderer.java
+++ declarative-knitting-core/src/main/java/com/griffiths/hugh/declarative_knitting/core/rendering/LatexRenderer.java
@@ -4,12 +4,16 @@ import com.griffiths.hugh.declarative_knitting.core.model.patterns.Pattern;
 import com.griffiths.hugh.declarative_knitting.core.model.patterns.PatternSegment;
 import com.griffiths.hugh.declarative_knitting.core.model.stitches.Stitch;
 import com.griffiths.hugh.declarative_knitting.core.rendering.util.RowCompressUtil;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
+import org.apache.commons.configuration.Configuration;
 
 /**
  * Renderer designed to render source code for the LaTeX KnittingPattern class:
@@ -18,10 +22,20 @@ import java.util.stream.Collectors;
  * This is intended to produce snippets to scaffold patterns as much as possible, it won't produce entire documents.
  */
 public class LatexRenderer implements Renderer, AutoCloseable {
+	public static final String LATEX_DOCUMENT_PROPERTIES = "/latex_document.properties";
 	private final PrintWriter printWriter;
+
+	private Properties config;
 
 	public LatexRenderer(OutputStream outputStream) {
 		this.printWriter = new PrintWriter(outputStream);
+
+		config = new Properties();
+		try {
+			config.load(this.getClass().getResourceAsStream(LATEX_DOCUMENT_PROPERTIES));
+		} catch (IOException e) {
+			throw new IllegalStateException("Configuration not found : "+LATEX_DOCUMENT_PROPERTIES);
+		}
 	}
 
 	@Override
@@ -32,43 +46,15 @@ public class LatexRenderer implements Renderer, AutoCloseable {
 	@Override
 	public void render(Pattern pattern) {
 		// Front matter
-		printWriter.println("\\documentclass{knittingpattern}\n" +
-				"\\usepackage{hyperref}\n\n" +
-				"% Commands\n" +
-				"% \\intro{Text}{pic}\n" +
-				"% \\note{borderColour}{backgroundColour}{Title}{Text}\n" +
-				"% \\diagram{diag}\n" +
-				"% \\begin{pattern}{colour1}{colour2} Col & (st) \\end{pattern}\n" +
-				"% \\important{borderColour}{backgroundColour}{Text}\n" +
-				"% \\biog{pic}{Text}\n" +
-				"% \\cpyrght{Name}\n" +
-				"\n");
+		printWriter.println(config.getProperty("title_page"));
 
-		printWriter.println(
-				"\\definecolor{colour0}{HTML}{000000}\n" +
-						"\\definecolor{colour1}{HTML}{6699FF}\n" +
-						"\\definecolor{colour2}{HTML}{CCCCCC}\n" +
-						"\\definecolor{colour3}{HTML}{FFFFAA}\n"
-		);
+		printWriter.println(config.getProperty("colour_definitions"));
 
-		printWriter.println(
-				"\\begin{document}\n" +
-						"\n" +
-						"\\title{Pattern Title}\n" +
-						"\\author{~~Author name}\n" +
-						"\\date{\\today}\n" +
-						"\\maketitle\n" +
-						"\n" +
-						String.format("\\cpyrght{\\copyright %s ~~Author name~~}\n", new SimpleDateFormat("yyyy").format(new Date())) +
-						"\n" +
-						"\\intro{\nDescribe your project here.}{lion.png}\n" +
-						"\n" +
-						"\\note{colour0}{colour3}{Materials}{\nDescribe materials - yarn, needles, and extras}\n");
+		printWriter.println(config.getProperty("introduction"));
 
 		pattern.getSegments().entrySet().forEach(entry -> {
 			// Print the title
-			printWriter.println(String.format("\\section*{%s}\n\n" +
-					"\\intro{\nDescription text, description text.\n}{lion.png}\n", entry.getKey()));
+			printWriter.println(String.format(config.getProperty("section_format"), entry.getKey()));
 
 			// Render the segment
 			render(entry.getValue());
@@ -76,8 +62,8 @@ public class LatexRenderer implements Renderer, AutoCloseable {
 		});
 
 		// End matter
-		printWriter.println("\\biog[.2]{lion.png}{Say something about yourself here!}\n" +
-				"\\end{document}");
+		printWriter.println(config.getProperty("biography"));
+		printWriter.println(config.getProperty("document_end"));
 	}
 
 	@Override
